994b646201d3ae30cfb2c67d85033a82bb86ca30
Роберт <robert.kos1234@gmail.com>
2020-10-18 00:51:54 +0300

    v1.1.0
    Добавил переводчик с английского языка.

--- vk-analiz/pars.py
+++ vk-analiz/pars.py
@@ -1,4 +1,12 @@
 import vk_api
+from googletrans import Translator
+
+
+def trans(en_word):
+    translator = Translator()
+    word = (translator.translate(en_word, dest='ru')).text
+    return word
+
 
 def user_anal(user_id):
     login = '+79185197432'
@@ -8,17 +16,36 @@ def user_anal(user_id):
     vk.auth()
     vk = vk.get_api()
     user_ids = user_id
-    user_info =vk.users.get(user_ids=user_ids)
+    user_info = vk.users.get(user_id=user_ids,fields = ['bdata','quotes'])
+    print(user_info)
+
+
+
+
+
+
     user_groups = vk.groups.get(user_id=user_ids,extended = 1, fields = 'description' )
 
     groups_bufer = user_groups['items']
-
+    x=0
     groups_name = []
     for i in groups_bufer:
-        groups_name.append(i['name'])
-
+        word = str(i['name'])
+        if word[0]<='z' and word[0]>='A':
+            word = trans(word)
+        groups_name.append(word)
+        if x<25:
+            x+=1
+        else:
+            break
+    x = 0
     groups_description = []
     for i in groups_bufer:
         groups_description.append(i['description'])
-
+        if x<25:
+            x+=1
+        else:
+            break
     return groups_name,groups_description
+
+print(user_anal(216824408))
