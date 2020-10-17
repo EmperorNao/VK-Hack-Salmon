f1ea8ac0b4497d4c171ccc00600591285720b67d
Роберт <robert.kos1234@gmail.com>
2020-10-17 18:48:15 +0300

    v1.0.0 рабочая штучка
    Допилил , готовы пока группы и описания групп

--- vk-analiz/pars.py
+++ vk-analiz/pars.py
@@ -1,42 +1,34 @@
 import vk_api
 
-login = '+79185197432'
-password = 'qy.pr.wppw'
+groups_name = list()
+groups_description = list()
+def user_anal(user_id):
+    login = '+79185197432'
+    password = 'qy.pr.wppw'
 
-vk = vk_api.VkApi(login,password,token='')
-vk_session = vk.auth()
-vk = vk.get_api()
-user_ids = 584194604
-user_info =vk.users.get(user_ids=user_ids)
+    vk = vk_api.VkApi(login,password,token='')
+    vk.auth()
+    vk = vk.get_api()
+    user_ids = user_id
+    user_info =vk.users.get(user_ids=user_ids)
+    user_groups = vk.groups.get(user_id=user_ids,extended = 1, fields = 'description' )
 
-print(user_info)
+    groups_bufer = user_groups['items']
 
-Dick_dict = dict()
+    groups_name = []
+    for i in groups_bufer:
+        groups_name.append(i['name'])
 
-##user_subscribe = vk.users.getSubscriptions(user_ids=user_ids)
-#print((user_subscribe))
-#array_keys = user_subscribe['groups']
-#array_keys = array_keys['items']
-#print(array_keys)
-
-
-user_groups = vk.groups.get(user_id=user_ids,extended = 1, fields = ['name','description'] )
-
-
-print(user_groups)
-
-x = user_groups['items']
-for i in x:
-    print('Название:',i['name'],'\n','Описание',i['description'])
-    print('_______________')
+    groups_description = []
+    for i in groups_bufer:
+        groups_description.append(i['description'])
 
 
 
 
-#for i in array_keys:
-#    tag_list = vk.groups.getByld(groups_id=1,fields ='name')
-#    print(tag_list)
 
 
-#for i in range(len(array_keys)):
- #   otsos_group(array_keys[i])
+
+user_anal(user_id = 216824408)
+print(groups_name)
+print(groups_description)
